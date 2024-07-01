889e84adb640104f8e6fbb31815c979c3b1f194a
rajanjha9235 <rajanjha9235@gmail.com>
2024-07-01 12:28:07 +0530

    set Up firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "react-chat-app-a698e.firebaseapp.com",
+  projectId: "react-chat-app-a698e",
+  storageBucket: "react-chat-app-a698e.appspot.com",
+  messagingSenderId: "1044347474525",
+  appId: "1:1044347474525:web:2396ec517b618020b733b4",
+  measurementId: "G-F71X0F0ZGC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
